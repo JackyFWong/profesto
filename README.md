911d3e4023dde6d1575a1db01f164f04bfa7178e
Jacky Wong <jwong2398@gmail.com>
2020-05-01 19:30:10 -0400

    finished functional app

--- README.md
+++ README.md
@@ -2,29 +2,37 @@
 Measures and visualizes computer productivity on Linux
 
 ## About the app
-STATUS = `data collection complete, gui on the way`
+STATUS = `functionally complete`
 
 'Profesto' is what I got from Google Translate when I translated 'workday' to Latin.
+
+Performance is not ideal. Due to the nature of matplotlib and the timeouts I have to use 
+to sync the program, there is stutter during data collection. Please allow a few seconds between 
+every (and I do mean every) interaction with the program.
 
 ## Dependencies
 - python3-xlib
 - psutil
 - matplotlib (only version 3.0.3)
 
-## Running the program
+## Installation for dependencies
+This will depend on your system. I use Fedora Linux.
+```bash
+dnf install python3-xlib
+pip3 install psutil
+pip3 install matplotlib==3.0.3
 ```
-nohup python3 /path/to/this/repo/app/main.py &
+
+## Running the program
+Due to the nature of the keyboard reader, you cannot run the program as a regular user. 
+You must use `sudo`. As such, I cannot provide a Bash script to easily execute the 
+program. In addition, because this is Python, there is no executable.
+```
+sudo nohup python3 app/main.py &
 ```
 You are free to close the terminal window afterwards.
 
-To close the program, find the process with
-```
-ps ax | grep main.py
-```
-and kill it.
-```
-kill [PID]
-```
+To close the program, close the window or click the Exit button.
 
 ## Data gathered
 - Keyboard data > reading the file `/dev/eventX`, where X is the keyboard device number
--- app/gui.py
+++ app/gui.py
@@ -1,6 +1,6 @@
 import os
 import queue
-import math
+from math import ceil
 import concurrent.futures as cf
 import PySimpleGUI as sg
 import matplotlib
@@ -22,6 +22,14 @@ def draw_figure(canvas, figure):
     fca.draw()
     fca.get_tk_widget().pack(side='top', fill='both', expand=1)
     return fca
+
+def update_win(new_win, old_win):
+    for key in new_win:
+        if key in old_win:
+            old_win[key] += new_win[key]
+        else:
+            old_win[key] = new_win[key]
+    return old_win
 
 class Gui:
     def __init__(self):
@@ -51,12 +59,12 @@ class Gui:
             gui_th2.result(), gui_th3.result())
 
     def launch_gui(self):
-        sg.theme('Material 1')
+        sg.theme('Dark Teal 6')
 
         mo_tab = [[sg.Canvas(size=(640,480), key='mo_canvas')]]
         kb_tab = [[sg.Canvas(size=(640,480), key='kb_canvas')]]
         sy_tab = [[sg.Canvas(size=(640,480), key='sy_canvas')]]
-        wn_tab = [[sg.Canvas(size=(640,480), key='wn_canvas')]]
+        wn_tab = [[sg.Multiline(size=(70,20), autoscroll=True, key='wn_canvas')]]
         graph_group = sg.TabGroup([[
             sg.Tab('Mouse', mo_tab),
             sg.Tab('Keyboard', kb_tab),
@@ -65,17 +73,18 @@ class Gui:
 
         data_col = [
                 [sg.Text('Overall Average Mouse Distance')],
-                [sg.Text('AAA', key="-MO_DIST-")],
+                [sg.Text('AAAAAAAAAAAAAAAB', key="-MO_DIST-")],
                 [sg.Text('Overall Average WPM')],
-                [sg.Text('BBB', key="-WPM_AVG-")],
+                [sg.Text('BBBBBBBBBBBBBBBB', key="-WPM_AVG-")],
                 [sg.Text('Ovearll Average CPU Usage')],
-                [sg.Text('CCC', key="-CPU_USG-")],
+                [sg.Text('CCCCCCCCCCCCCCCC', key="-CPU_USG-")],
                 [sg.Text('Current Memory Usage')],
-                [sg.Text('DDD / EEE', key="-MEM_USG-")],
+                [sg.Text('DDDDDDDDDDDDDDDD / EEEEEEEEEEEEEEEE', key="-MEM_USG-")],
                 [sg.Text('Total Disk Usage')],
-                [sg.Text('FFF / GGG', key="-DISK_USG-")],
+                [sg.Text('FFFFFFFFFFFFFFFF / GGGGGGGGGGGGGGGG', key="-DISK_USG-")],
                 [sg.Text('Total Network usage')],
-                [sg.Text('HHH / III', key="-NETW_USG-")]
+                [sg.Text('HHHHHHHHHHHHHHHH / IIIIIIIIIIIIIIII', key="-NETW_USG-")],
+                [sg.Button('Clear history', button_color=('black','red'), key="-CLEAR-")]
         ]
 
         layout = [
@@ -94,22 +103,22 @@ class Gui:
         mo_hist = [mo_data['dist']]
         kb_hist = [kb_data['wpm']]
         sy_hist = [sy_data['total_cpu_per']]
+        wn_hist = wn_data
+        for k in wn_hist:
+            wn_hist[k] = ceil(wn_hist[k])
 
         # mpl setup
         mo_canvas_elem = window['mo_canvas']
         kb_canvas_elem = window['kb_canvas']
         sy_canvas_elem = window['sy_canvas']
-        #wn_canvas_elem = window['wn_canvas']
         
         mo_fig = plt.figure(0)
         kb_fig = plt.figure(1)
         sy_fig = plt.figure(2)
-        #wn_fig = plt.figure(3)
 
         mo_ax = mo_fig.add_subplot(111)
         kb_ax = kb_fig.add_subplot(111)
         sy_ax = sy_fig.add_subplot(111)
-        #wn_ax = wn_fig.add_subplot(111)
 
         mo_agg = draw_figure(mo_canvas_elem.TKCanvas, mo_fig)
         kb_agg = draw_figure(kb_canvas_elem.TKCanvas, kb_fig)
@@ -121,16 +130,17 @@ class Gui:
             kb_total_avg = sum(kb_hist) / len(kb_hist)
             cpu_total_avg = sum(sy_hist) / len(sy_hist)
 
-            window['-MO_DIST-'].update(f"{math.ceil(mo_total_avg)}pixels")
-            window['-WPM_AVG-'].update(f"{math.ceil(kb_total_avg)}WPM")
-            window['-CPU_USG-'].update(f"{cpu_total_avg}%")
+            window['-MO_DIST-'].update(f"{ceil(mo_total_avg)} pixels")
+            window['-WPM_AVG-'].update(f"{ceil(kb_total_avg)} WPM")
+            window['-CPU_USG-'].update(f"{ceil(cpu_total_avg)}%")
             window['-MEM_USG-'].update(f"{sy_data['mem_per']}% virtual" +
-                f"{sy_data['swap_per']}% swap")
+                f" | {sy_data['swap_per']}% swap")
             window['-DISK_USG-'].update(f"{sy_data['disk_read']} read" +
-                f"{sy_data['disk_write']} write")
+                f" | {sy_data['disk_write']} write")
             window['-NETW_USG-'].update(f"{sy_data['net_sent']} sent" +
-                f"{sy_data['net_recv']} recieved")
+                f" | {sy_data['net_recv']} recieved")
 
+            # update graphs
             mo_ax.cla()
             mo_ax.plot(time, mo_hist)
             mo_agg.draw()
@@ -143,26 +153,31 @@ class Gui:
             sy_ax.plot(time, sy_hist)
             sy_agg.draw()
 
+            # update windows
+            out_str = ""
+            wn_hist = {k: v for k, v in sorted(wn_hist.items(), key=lambda item: item[1], reverse=True)}
+            for key in wn_hist:
+                out_str += f"{key} : {ceil(wn_hist[key])}\n"
+            window['wn_canvas'].print(f"\n{out_str}")
+
             event, values = window.read(timeout=100)
 
             if event in (None, 'Exit'):
                 break
+            if event == '-CLEAR-':
+                mo_hist = [0]
+                kb_hist = [0]
+                sy_hist = [0]
+                wn_hist = {}
+                time = [0]
 
             (mo_data, kb_data, sy_data, wn_data) = self.get_data(update_interval)
             time.append(time[-1]+update_interval)
             mo_hist.append(mo_data['dist'])
             kb_hist.append(kb_data['wpm'])
             sy_hist.append(sy_data['total_cpu_per'])
+            wn_hist = update_win(wn_data, wn_hist)
 
-            """
-            mo_line.set_xdata(time)
-            mo_line.set_ydata(mo_data['dist'])
-            kb_line.set_xdata(time)
-            kb_line.set_ydata(kb_data['wpm'])
-            sy_line.set_xdata(time)
-            sy_line.set_ydata(sy_data['total_cpu_per'])
-            """
-            
             
         # user exits window
         window.close()
